b202594c7b90e045c04b0c2bda8b88c2e0aa1f12
Aaron Parecki <aaron@parecki.com>
2019-02-27 11:40:47 -0800

    update example config with notes

--- env.example.php
+++ env.example.php
@@ -1,5 +1,8 @@
 <?php
-define('OKTA_BASE_URL', 'https://dev-000000.oktapreview.com');
+define('OKTA_BASE_URL', 'https://youroktadomain.okta.com');
 define('OKTA_CLIENT_ID', '');
 define('OKTA_CLIENT_SECRET', '');
-define('OKTA_AUTH_SERVER_ID', 'default');
+
+# If you're using API Access Management, define the auth server ID below.
+# Otherwise leave it commented out.
+# define('OKTA_AUTH_SERVER_ID', 'default');
