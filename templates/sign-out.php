017b1f7b384b188e148ce2acc02d6db09adb142a
Aaron Parecki <aaron@parecki.com>
2021-09-30 16:56:40 -0700

    better fix for logout

--- templates/sign-out.php
+++ templates/sign-out.php
@@ -3,14 +3,14 @@
 <script>
 <?php include plugin_dir_path(__FILE__).'/../includes/initialize-widget.js.php'; ?>
 
-oktaSignIn.authClient.token.getUserInfo().then(function(user) {
-    oktaSignIn.authClient.tokenManager.get('idToken').then(function(idToken){
-        oktaSignIn.authClient.signOut({
-            idToken: idToken,
-            postLogoutRedirectUri: '<?php echo home_url() ?>'
-        });
-    });
-}, function(error) {
-    window.location = '<?php echo home_url() ?>';
+oktaSignIn.authClient.tokenManager.get('idToken').then(function(idToken){
+    if(idToken) {
+      oktaSignIn.authClient.signOut({
+          idToken: idToken,
+          postLogoutRedirectUri: '<?php echo home_url() ?>'
+      });
+    } else {
+      window.location = '<?php echo home_url() ?>';
+    }
 });
 </script>
